8e627e91709ee13584472fef564be62796d3d9d6
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-01-11 16:23:45 -0300

    adicionado projeto final

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #ff7a00;
+    color: #FFF;
+    border: 1px solid #ff7a00;
+    border-radius: 21px;
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
